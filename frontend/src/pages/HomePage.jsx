d2694e6d0c84c1c65988fdcb1054d5c94cb100fb
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-09-11 13:25:39 +0200

    Added initial input section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/Header";
+import { InputSection } from "../components/InputSection/InputSection";
 
 export const Homepage = () => {
   return (
     <div>
       <Header text="JeppeKPT" />
+      <InputSection />
     </div>
   );
 };
