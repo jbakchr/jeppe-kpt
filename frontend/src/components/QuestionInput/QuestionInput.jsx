8b53234d1eb36c0b371be353896c85f71db8c69c
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-09-11 14:17:38 +0200

    Fixed QuestionInput styling

--- frontend/src/components/QuestionInput/QuestionInput.jsx
+++ frontend/src/components/QuestionInput/QuestionInput.jsx
@@ -1,18 +1,9 @@
+import "./QuestionInput.css";
+
 export const QuestionInput = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "lightgray",
-        width: "600px",
-        height: "40px",
-        borderRadius: "20px",
-        padding: "0 10px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <input type="text" />
+    <div className="question-input">
+      <input type="text" style={{ flexGrow: 1 }} />
       <p>icon</p>
     </div>
   );
