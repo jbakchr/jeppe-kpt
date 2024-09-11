2d57e980dc60e4a19bf90c941091a38351fac465
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-09-11 18:16:03 +0200

    Added rendering of question and answers

--- frontend/src/components/QuestionAndAnswerList/QuestionAndAnswerList.jsx
+++ frontend/src/components/QuestionAndAnswerList/QuestionAndAnswerList.jsx
@@ -5,7 +5,27 @@ import { responseContext } from "../../context/ResponseContext";
 export const QuestionAndAnswerList = () => {
   const { questionAndAnswers } = useContext(responseContext);
 
-  console.log(questionAndAnswers);
+  const renderQuestionAndAnswers = () => {
+    return questionAndAnswers.map((item, index) => {
+      if (item.question) {
+        return (
+          <div key={index}>
+            <p>{item.question}</p>
+          </div>
+        );
+      } else {
+        return (
+          <div key={index}>
+            <p>{item.answer}</p>
+          </div>
+        );
+      }
+    });
+  };
 
-  return <div>QuestionAndAnswerList</div>;
+  if (questionAndAnswers.length == 0) {
+    return null;
+  }
+
+  return <div>{renderQuestionAndAnswers()}</div>;
 };
