c0daf998855076e4eb85685c140f174f45ab7fc6
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-09-11 17:03:33 +0200

    Added openai answers to backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import OpenAI from "openai";
 
 const PORT = 3000;
 
@@ -8,14 +9,32 @@ dotenv.config();
 
 const app = express();
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 app.use(cors());
 app.use(express.json());
 
-app.post("/api/question", (req, res) => {
+app.post("/api/question", async (req, res) => {
   const { question } = req.body;
-  const { OPENAI_API_KEY } = process.env;
 
-  res.json({ message: question });
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "Du skal svare som karakteren Jeppe K fra Danish Dynamite. Husk at inkludere hans grin i dine svar",
+      },
+      {
+        role: "user",
+        content: question,
+      },
+    ],
+  });
+
+  res.json({ answer: completion.choices[0].message.content });
 });
 
 app.listen(PORT, () => {
