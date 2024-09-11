da81412086fad24a5a7b4372987af415e76f730b
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-09-11 14:25:42 +0200

    Added QuestionInput styling

--- frontend/src/components/QuestionInput/QuestionInput.jsx
+++ frontend/src/components/QuestionInput/QuestionInput.jsx
@@ -3,7 +3,16 @@ import "./QuestionInput.css";
 export const QuestionInput = () => {
   return (
     <div className="question-input">
-      <input type="text" style={{ flexGrow: 1 }} />
+      <input
+        type="text"
+        style={{
+          flexGrow: 1,
+          margin: "10px",
+          border: "none",
+          background: "none",
+          outline: "none",
+        }}
+      />
       <p>icon</p>
     </div>
   );
